fc5cea459d1d5468744c4a73291ceb2b59f68214
Pr0fB0ff <msereboff@gmail.com>
2017-11-20 18:31:59 -0800

    Initial Commit

---
+++ InClass0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InClass0
+//
+//  Created by Marcel on 11/20/17.
+//  Copyright © 2017 Marcel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
